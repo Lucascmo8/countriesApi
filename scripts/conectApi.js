c7a992c42c5583673d50a17251f544d8c5f93002
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2023-02-01 19:09:47 -0300

    Conexão com a API

---
+++ scripts/conectApi.js
@@ -0,0 +1,26 @@
+let lugarCerto = document.getElementById("countriesSection")
+
+let contries = []
+const urlApi = 'https://restcountries.com/v2/all'
+
+teste()
+
+async function teste(){
+    const res = await fetch(urlApi)
+    contries = await res.json()
+    console.log(contries)
+    console.log(contries.length)
+    contries.forEach(country => lugarCerto.innerHTML += `
+    <div class="countryCard">
+        <div class="CountryImg">
+            <img src="${country.flag}" alt="${country.name} flag">
+        </div>
+        <div class="countryText\">    
+            <h2>${country.name}</h2>
+            <p><strong>Population:</strong> ${country.population}</p>
+            <p><strong>Region:</strong> ${country.region}</p>
+            <p><strong>Capital:</strong> ${country.capital}</p>
+        </div>
+    </,div>`);
+}
+
