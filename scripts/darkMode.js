053282f98f7a51018de27c9006fd6be5f901771b
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2023-02-01 21:19:06 -0300

    dark mode para os cards

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -2,8 +2,9 @@ let body = document.body
 let header = document.getElementById("headerMenu")
 let main = document.getElementById("main")
 let btnDarkAndLightMode = document.getElementById("btnDarkAndLightMode")
+let countriesSection = document.getElementById("countriesSection")
 
-const elements=[body,header,main]
+const elements=[body,header,main,countriesSection]
 
 btnDarkAndLightMode.addEventListener("click",switchColor)
 
