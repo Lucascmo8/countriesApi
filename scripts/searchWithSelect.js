e27c61de11f9866ff492e9ab1293cbf5f9c7ad9f
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2023-02-02 19:19:09 -0300

    teste 3 com onclick no html

--- scripts/searchWithSelect.js
+++ scripts/searchWithSelect.js
@@ -1,15 +1,16 @@
 // import { countriesSection } from "./conectApi.js";
 import { countries } from "./conectApi.js"
 import { shownCardsWithCountries } from "./showCards.js"
-// let options = document.querySelectorAll("[data-option]")
+let options = document.querySelectorAll("[data-option]")
 let SelectAboutRegion = document.querySelector("#SelectAboutRegion")
+console.log(SelectAboutRegion)
 SelectAboutRegion.addEventListener("click",filterRegion)
 
-SelectAboutRegion.addEventListener("blur",filterRegion)
+// options.addEventListener("click",filterRegion)
+console.log(options)
 
 let regionSelected = ["all"]
 function filterRegion(){
-    
     if(regionSelected[0] !== SelectAboutRegion.value){
         regionSelected.shift()
         regionSelected.push(SelectAboutRegion.value)
@@ -17,6 +18,7 @@ function filterRegion(){
     }
 
 }
+
 
 function showByRegion(region){
     if(region == "all"){
